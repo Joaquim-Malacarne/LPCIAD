749abc10d9119500af387122fc03e21446b3dd06
Joca <joaquim.malacarne013@gmail.com>
2026-03-08 19:09:02 -0300

    feat -> adição de markdown en/pt

--- frontend/LPCIAD_Web/src/app/app.routes.ts
+++ frontend/LPCIAD_Web/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { Home } from './home/home';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: Home },
+  {
+    path: 'admin/add-post',
+    loadComponent: () =>
+      import('./admin/add-post/add-post').then(m => m.AddPost)
+  },
   { path: '**', redirectTo: '' }
 ];
-
